43556e1d97041558712214e957d08eea78ee8995
edoardobasso <57095258+edoardobasso@users.noreply.github.com>
2020-01-31 17:50:27 +0100

    Delete googleKey.js

--- config/googleKey.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    google: {
-        googleid: "609240756031-vo7g5riq8f2tiloeopgbktelnjoo6flb.apps.googleusercontent.com",
-        secret: "AP3XhKT9PAMSb6hJKN9JuN5x",
-    }
-}
